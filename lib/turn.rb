3f640cf9655f553e78057cb85b289a40c1eae90b
Keginold Philip <keginoldp@hotmail.com>
2020-12-10 15:31:19 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,36 @@
+
+
+def display_board(board)
+   puts " #{board[0]} | #{board[1]} | #{board[2]} "
+   puts "-----------"
+   puts " #{board[3]} | #{board[4]} | #{board[5]} "
+   puts "-----------"
+   puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  index = user_input.to_i - 1
+end
+
+def valid_move?(board, index)
+  if index.between?(0,8) && !position_taken?(board, index)
+    true
+  end
+end
+
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    false
+  else
+    true
+  end
+end
+
+
+def move(board, index, player)
+  board[index] = player
+end
+
+def turn(board)
+
+end
